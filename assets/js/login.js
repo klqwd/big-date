5c149c8c5b4fa35270e7a15252a4b50c8e85f0b7
itheima <itheima@itcast.cn>
2022-08-02 15:49:12 +0800

    2

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,80 +1,83 @@
-$(function () {
-  // 点击去注册账号让 登录框隐藏，注册框显示
-  $("#link_reg").on("click", function () {
-    $(".login-box").hide();
-    $(".reg-box").show();
-  });
+$('#link_reg').on('click', function () {
+  $('.login-box').hide()
+  $('.reg-box').show()
+})
 
-  $("#link_login").on("click", function () {
-    $(".reg-box").hide();
-    $(".login-box").show();
-  });
-});
+$('#link_login').on('click', function () {
+  $('.login-box').show()
+  $('.reg-box').hide()
+})
 
 // 从 LayUI 中获取 form 对象
-const form = layui.form;
+const form = layui.form
+
+const baseUrl = 'http://www.liulongbin.top:3007'
+
+const layer = layui.layer
 
 // 通过 form.verify() 方法自定义校验规则
 form.verify({
-  // 自定义一个叫 pwd 的校验规则
-  pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
   // 校验两次密码是否一致的规则
-  repwd: (val) => {
-    // 通过形参拿到的是确认密码框中的内容
-    // 还需要拿到密码框中的内容
-    // 然后进行一次等于的判断
-    // 如果判断失败,则return一个提示消息即可
-    const pwd = $(".reg-box [name=password").val();
-    if (pwd !== val) return "两次密码不一致";
+  repass: value => {
+      // value：表单的值 item：表单的dom对象
+      // [:jQuery的属性选择器，用来判断两次输入的密码是否一致
+      const pwd = $('.reg-box [name=password').val()
+      if (pwd != value) return "两次密码不一致"
   },
+  //我们既支持上述函数式的方式，也支持下述数组的形式
+  //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+  pass: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格']
 });
 
-const layer = layui.layer;
-const baseUrl = "http://www.liulongbin.top:3007";
-
-// 监听注册表单，发送注册请求
-$("#form_reg").on("submit", (e) => {
-  console.log(1);
+$('#form_reg').on('submit', function (e) {
+  // 阻止默认提交行为
   e.preventDefault();
+  // 获取表单全部属性
+  let data = $(this).serialize()
+  // 发送请求
   $.ajax({
-    type: "POST",
-    url: baseUrl + "/api/reguser",
-    data: {
-      username: $("#form_reg [name=username").val(),
-      password: $("#form_reg [name=password").val(),
-    },
-    success: (res) => {
-      if (res.status !== 0) return layer.msg(res.message);
-      layer.msg("注册成功！");
-      // 注册成功后跳转到登录界面
-      $("#link_login").click();
-    },
-  });
-});
-// 获取 layui 弹窗
+      type: 'POST',
+      url: baseUrl + '/api/reguser',
+      data,
+      success: res => {
+          const { status, message } = res
+          if (status !== 0) return layer.msg(message)
+          $('#link_login').click()
+      }
+  })
+})
 
-// 监听登录表单，发送登录请求
-$("#form_login").submit((e) => {
+
+$('#form_login').on('submit', function (e) {
+  // 阻止默认提交行为
   e.preventDefault();
+  // 获取表单全部属性
+  const data = $(this).serialize()
+  // 发送请求
   $.ajax({
-      type: "POST",
-      url: baseUrl + "/api/login",
-      data: $("#form_login").serialize(),
-      success: (res) => {
-          if (res.status !== 0) return layer.msg(res.message);
-          layer.msg("登录成功！");
+      type: 'POST',
+      url: baseUrl + '/api/login',
+      data,
+      success: res => {
+          const { status, message,token } = res
+
+          if (status !== 0) return layer.msg(message)
           // 将登录成功得到的 token 字符串，保存到 localStorage 中
-          localStorage.setItem("token", res.token);
+          localStorage.setItem('token',token)
           // 跳转到主页
-          location.href = "/index.html";
-      },
-  });
-});
+          location.href = '/index.html'
+      }
+  })
+})
 
-// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option) => {
-  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  option.url = `http://www.liulongbin.top:3007` + option.url;
-});
+
+
+
+  // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+  // 会先调用 ajaxPrefilter 这个函数
+  // 在这个函数中，可以拿到我们给Ajax提供的配置对象
+  $.ajaxPrefilter((option) => {
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    option.url = `http://www.liulongbin.top:3007` + option.url;
+  });
+
